cc92991bbc08daacc0188afe4edb40f50bcd2ea2
Eshandwivedi13 <79198616+Eshandwivedi13@users.noreply.github.com>
2024-04-25 14:22:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+// window.location.origin;
+// console.log(window.location);
+// const currentURL = window.location.href;
+// console.log(currentURL, "a");
+
+let url = window.location.ancestorOrigins[1];
+let n = url.length;
+alert(`The length of the URL is : ${n}`);
