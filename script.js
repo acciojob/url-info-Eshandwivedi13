cea41f71a0da9c5b151d812c61f19a46a3b3ee06
Eshandwivedi13 <79198616+Eshandwivedi13@users.noreply.github.com>
2024-04-25 14:33:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 // console.log(url, window.location)
-let currentURL  = window.location.href;//href property gives curUrl
-let urlLength  = currentURL.length;
-alert(`The length of the URL is : ${urlLength}`); 
+const currentURL = window.location.href;
+
+// Get the length of the URL
+const urlLength = currentURL.length;
+
+// Print the length of the URL using alert
+alert(`The length of the URL is: ${urlLength}`);
