1c086f4e041994ca383c336471dd28a858fb696e
Eshandwivedi13 <79198616+Eshandwivedi13@users.noreply.github.com>
2024-04-25 14:26:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-//your JS code here. If required.
-// window.location.origin;
-// console.log(window.location);
-// const currentURL = window.location.href;
-// console.log(currentURL, "a");
-
-let url = window.location.ancestorOrigins[1];
+// console.log(url, window.location)
+let url = window.location.href;//href property gives curUrl
 let n = url.length;
 alert(`The length of the URL is : ${n}`);
